e2db70eed91c52d3ff95c205b0d13f8e5e91efe6
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-07-30 16:17:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -65,7 +65,7 @@ var config = {
                 bearing: 0
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [
                 // {
@@ -95,7 +95,7 @@ var config = {
                 bearing: 0
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [
                 // {
